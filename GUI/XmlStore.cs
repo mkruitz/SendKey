9c14cc0db4a51a211da84f5eb39628cc27182862
Martin Kruitz <martin.kruitz@hightechict.nl>
2014-12-05 15:23:55 +0100

    Implemented XmlStore

---
+++ GUI/XmlStore.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Core;
+
+namespace GUI
+{
+    public class XmlStore : IStore
+    {
+        private const string StoreLocation = "store.xml";
+
+        public IList<ScanCommmands> AllCommands { get { return ReadFromFile(); } }
+
+        private IList<ScanCommmands> ReadFromFile()
+        {
+            var commands = new List<ScanCommmands>();
+            if (!File.Exists(StoreLocation))
+                return commands;
+
+            var document = XDocument.Load(StoreLocation);
+            return ListFromXml(document.Root);
+        }
+
+        private XElement ToXml(IEnumerable<ScanCommmands> allCommands)
+        {
+            return new XElement("Commands", allCommands.Select(ToXml));
+        }
+
+        private XElement ToXml(ScanCommmands scanCommmands)
+        {
+            var flatttendKeysToSend = String.Join(Environment.NewLine, scanCommmands.KeysToSend);
+            return new XElement(
+                "ScanCommand",
+                new XAttribute("DisplayName", scanCommmands.DisplayName),
+                new XAttribute("ProcessName", scanCommmands.ProcessName),
+                new XAttribute("TitleStartsWith", scanCommmands.TitleStartsWith),
+                flatttendKeysToSend
+                );
+        }
+
+        private IList<ScanCommmands> ListFromXml(XElement element)
+        {
+            return element.Elements("ScanCommand")
+                .Select(ItemFromXml)
+                .ToList();
+        }
+
+        private ScanCommmands ItemFromXml(XElement element)
+        {
+            var keysToSend =
+                element.Value.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).ToList();
+            return new ScanCommmands{
+                DisplayName = element.Attribute("DisplayName").Value,
+                ProcessName = element.Attribute("ProcessName").Value,
+                TitleStartsWith = element.Attribute("TitleStartsWith").Value,
+                KeysToSend = keysToSend
+            };
+        }
+
+        public void Save(ScanCommmands scanCommmands)
+        {
+            var list = AllCommands;
+
+            if (!list.Contains(scanCommmands))
+            {
+            list.Insert(0, scanCommmands);
+            ToXml(list)
+                .Save(StoreLocation, SaveOptions.None);
+            }
+        }
+    }
+}
---
+++ Tests/XmlStoreTests.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using Core;
+using GUI;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class XmlStoreTests : StoreBaseTests
+    {
+        private IStore _store;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _store = new XmlStore();
+            if (File.Exists("store.xml"))
+            {
+                File.Delete("store.xml");
+            }
+        }
+
+        [Test]
+        public void CreateNewStore_GetAllCommands_NonInStore()
+        {
+            CreateNewStore_GetAllCommands_NonInStore(_store);
+        }
+
+        [Test]
+        public void CreateNewStore_AddOneItem_OneItemInStore()
+        {
+            CreateNewStore_AddOneItem_OneItemInStore(_store);
+        }
+
+        [Test]
+        public void CreateSecondStoreWithXmlOnSameLocation_AddOneItem_ReadOneItemFromSecondStore()
+        {
+            CreateNewStore_AddOneItem_OneItemInStore(_store);
+
+            var secondStore = new XmlStore();
+            Assert.AreEqual(1, secondStore.AllCommands.Count);
+        }
+
+        [Test]
+        public void CreateNewStore_TwoDifferentItems_TwoItemsInStoreLastAddedItemIsFirstInList()
+        {
+            CreateNewStore_TwoDifferentItems_TwoItemsInStoreLastAddedItemIsFirstInList(_store);
+        }
+
+        [Test]
+        public void CreateNewStore_TwoDifferentItemsSameInternals_OneItem()
+        {
+            CreateNewStore_TwoDifferentItemsSameInternals_OneItem(_store);
+        }
+    }
+}
